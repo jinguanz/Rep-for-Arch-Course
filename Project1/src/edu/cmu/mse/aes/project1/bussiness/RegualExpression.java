8fe97f01e3b869f0bf2368a02677948db0a839f0
ruili <ruiliwonderful@gmail.com>
2012-09-18 13:45:37 -0400

    data fetcher done

---
+++ Project1/src/edu/cmu/mse/aes/project1/bussiness/RegualExpression.java
@@ -0,0 +1,16 @@
+package edu.cmu.mse.aes.project1.bussiness;
+/*
+ * author: Rui Li
+ * 
+ * This class will keep the regx which will be used for filter out the useful info
+ */
+public class RegualExpression {
+	
+	private  final String RegxForFilterLinks="";
+	
+	
+
+	
+	
+
+}
