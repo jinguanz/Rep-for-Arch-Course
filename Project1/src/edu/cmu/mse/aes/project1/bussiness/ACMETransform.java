183a071ea27caacb5528ea29fb7002389ace3900
ruili <ruiliwonderful@gmail.com>
2012-09-25 22:12:28 -0400

    update with console

--- Project1/src/edu/cmu/mse/aes/project1/bussiness/ACMETransform.java
+++ Project1/src/edu/cmu/mse/aes/project1/bussiness/ACMETransform.java
@@ -6,54 +6,116 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
-public class ACMETransform {
+public class ACMETransform implements IQueryService{
 
-    /**
-     * Performs an XSLT transformation, sending the results
-     * to source.htm.
-     */
-    public static void main(String[] args) throws Exception {
-        if (args.length != 3) {
-            System.err.println(
-                "Usage: java Transform [xmlfile] [xsltfile]");
-            System.exit(1);
-        }
-        
-        String brand = "";
-        String model = "";
-        
-        //System.out.println("Enter a brand: ");
-        //Scanner scan = new Scanner(System.in);
-        //brand = scan.next();
-        //System.out.println("You entered the following brand: "+brand);
-        
-        //System.out.println("Enter a model: ");
-        //model = scan.next();
-        model = args[2];
-        //System.out.println("You entered the following model: "+model);
-        //scan.close();
-        
-        File xmlFile = new File(args[0]);
-        File xsltFile = new File(args[1]);
+	private final String xslFileName = "data/ACMEBikeList.xsl";
+	private final String sourceFileName = "data/integratedXMLFile.xml";
+	private final String HtmlFile = "data/source.htm";
+	private static HashMap<String, ArrayList<String>> brandModelMap = new HashMap<String, ArrayList<String>>();
 
-        File f = new File("C:/Users/DELL/git/Rep-for-Arch-Course/Project1/src/outputdata/source.htm");
-        //BufferedWriter bw = new BufferedWriter(new FileWriter(f));
-        
-        Source xmlSource = new StreamSource(xmlFile);
-        Source xsltSource = new StreamSource(xsltFile);
+	ACMETransform(){
+		super();
+	}
+	
+	ACMETransform(HashMap map) {
+		super();
+		this.brandModelMap = map;
+	}
 
-        TransformerFactory transFact =
-                TransformerFactory.newInstance();
-        Transformer trans = transFact.newTransformer(xsltSource);
-        trans.setParameter("modelname", model.toString());
+	public void supportBrands() {
+		
+		for(String brand:brandModelMap.keySet()){
+			System.out.println(brand);
+			
+		}
+	}
 
-        trans.transform(xmlSource, new StreamResult(new FileOutputStream(f)));
-        //trans.transform(xmlSource, new StreamResult(bw));
-        //String url = "http://www.google.com";
-        java.awt.Desktop.getDesktop().browse(java.net.URI.create("file:///C:/Users/DELL/git/Rep-for-Arch-Course/Project1/src/outputdata/source.htm"));
-        
-        System.out.println("Processing done!!");
-    }
+	
+	public void supportedModels(String brand) {
+		ArrayList<String> models=brandModelMap.get(brand);
+		int i=1;
+		for(String model:models){
+			System.out.println(i+"."+model);
+			i++;
+		}
+	}
+
+	
+	public void viewCertainModel(String model) {
+
+		File f = new File(HtmlFile);
+		File sourceFile = new File(sourceFileName);
+		File xslFile = new File(xslFileName);
+		// BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+
+		Source xmlSource = new StreamSource(sourceFile);
+		Source xsltSource = new StreamSource(xslFile);
+
+		TransformerFactory transFact = TransformerFactory.newInstance();
+		Transformer trans;
+		try {
+			trans = transFact.newTransformer(xsltSource);
+
+			trans.setParameter("modelname", model.toString());
+
+			trans.transform(xmlSource,
+					new StreamResult(new FileOutputStream(f)));
+			// trans.transform(xmlSource, new StreamResult(bw));
+			// String url = "http://www.google.com";
+			java.awt.Desktop.getDesktop().browse(java.net.URI.create(HtmlFile));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Processing done!!");
+
+	}
+
+//	public static void main(String[] args) throws Exception {
+//		if (args.length != 3) {
+//			System.err.println("Usage: java Transform [xmlfile] [xsltfile]");
+//			System.exit(1);
+//		}
+//
+//		String brand = "";
+//		String model = "";
+//
+//		final String HtmlFile = "data/source.htm";
+//		final String transformedHTML = "data/source.htm";
+//		// System.out.println("Enter a brand: ");
+//		// Scanner scan = new Scanner(System.in);
+//		// brand = scan.next();
+//		// System.out.println("You entered the following brand: "+brand);
+//
+//		// System.out.println("Enter a model: ");
+//		// model = scan.next();
+//		model = args[2];
+//		// System.out.println("You entered the following model: "+model);
+//		// scan.close();
+//
+//		File xmlFile = new File(args[0]);
+//		File xsltFile = new File(args[1]);
+//
+//		File f = new File(HtmlFile);
+//		// BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+//
+//		Source xmlSource = new StreamSource(xmlFile);
+//		Source xsltSource = new StreamSource(xsltFile);
+//
+//		TransformerFactory transFact = TransformerFactory.newInstance();
+//		Transformer trans = transFact.newTransformer(xsltSource);
+//		trans.setParameter("modelname", model.toString());
+//
+//		trans.transform(xmlSource, new StreamResult(new FileOutputStream(f)));
+//		// trans.transform(xmlSource, new StreamResult(bw));
+//		// String url = "http://www.google.com";
+//		java.awt.Desktop.getDesktop().browse(
+//				java.net.URI.create(transformedHTML));
+//
+//		System.out.println("Processing done!!");
+//	}
 }
--- Project1/src/edu/cmu/mse/aes/project1/bussiness/IQueryService.java
+++ Project1/src/edu/cmu/mse/aes/project1/bussiness/IQueryService.java
@@ -1,5 +1,14 @@
 package edu.cmu.mse.aes.project1.bussiness;
 
+/*
+ * @author: Rui Li
+ */
+
 public interface IQueryService {
+	public void supportBrands();
+
+	public void supportedModels(String brand);
+
+	public void viewCertainModel(String model);
 
 }
