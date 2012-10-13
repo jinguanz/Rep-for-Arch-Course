75a6672be6c40e522f00a2323a9e550b4fb11017
Aadish Kotwal <kotwal.aadish@gmail.com>
2012-10-12 22:59:57 -0400

    Client request and response message types

---
+++ Project2/MessageDef/src/edu/cmu/mse/project2/data/ClientReadDetailsOfAModelResponse.java
@@ -0,0 +1,64 @@
+package edu.cmu.mse.project2.data;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.jboss.soa.esb.message.Attachment;
+import org.jboss.soa.esb.message.Body;
+import org.jboss.soa.esb.message.Context;
+import org.jboss.soa.esb.message.Fault;
+import org.jboss.soa.esb.message.Header;
+import org.jboss.soa.esb.message.Message;
+import org.jboss.soa.esb.message.Properties;
+
+public class ClientReadDetailsOfAModelResponse implements Message {
+
+	@Override
+	public Message copy() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Attachment getAttachment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Body getBody() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Context getContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Fault getFault() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Header getHeader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Properties getProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public URI getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
