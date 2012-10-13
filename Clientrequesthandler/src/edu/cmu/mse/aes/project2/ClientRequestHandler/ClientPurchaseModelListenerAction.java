a08613fa3dd56d5bc78530ab1618817418635d33
Aadish Kotwal <kotwal.aadish@gmail.com>
2012-10-12 23:04:14 -0400

    ClientRequestHandler stub generated.

---
+++ Clientrequesthandler/src/edu/cmu/mse/aes/project2/ClientRequestHandler/ClientPurchaseModelListenerAction.java
@@ -0,0 +1,7 @@
+package edu.cmu.mse.aes.project2.ClientRequestHandler;
+
+import org.jboss.soa.esb.actions.AbstractActionLifecycle;
+
+public class ClientPurchaseModelListenerAction extends AbstractActionLifecycle {
+
+}
